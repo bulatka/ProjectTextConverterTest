1d4faaf33326a3a278e3c452e7a4727fbae4af85
Bulat Sabitov <305264@gmail.com>
2022-03-18 10:23:10 +0300

    getter setter

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,4 +1,20 @@
 public class GitTest {
     private String s;
     private StringBuilder sb;
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public void setSb(StringBuilder sb) {
+        this.sb = sb;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public StringBuilder getSb() {
+        return sb;
+    }
 }
