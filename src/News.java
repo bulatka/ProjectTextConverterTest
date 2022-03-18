d2b300ec4dfe08275835e083bcc09bd4564e82b0
Bulat Sabitov <305264@gmail.com>
2022-03-18 12:58:20 +0300

    News Comparator

--- src/News.java
+++ src/News.java
@@ -1,2 +1,9 @@
-public class News {
+import java.util.Comparator;
+
+public class News<String> implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        return compare(o1, o2);
+    }
 }
